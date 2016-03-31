5e3e1175e8c0a94c56a42dd6d56b4531c0bf1b72
Martin <martin@theamalgama.com>
2016-03-31 15:29:10 -0300

    Hello world

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 31, 2016
+ *      Author: mastanca
+ */
+
+#include <iostream>
+
+int main(){
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
+
+
