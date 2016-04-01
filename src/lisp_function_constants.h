6f44d467826afdc51fb5eaaf4490f6e94710809d
Martin Stancanelli <mastanca14@gmail.com>
2016-04-01 20:23:06 -0300

    Define lisp constants

---
+++ src/lisp_function_constants.h
@@ -0,0 +1,30 @@
+/*
+ * lisp_function_constants.h
+ *
+ *  Created on: Apr 1, 2016
+ *      Author: mastanca
+ */
+
+#ifndef SRC_LISP_FUNCTION_CONSTANTS_H_
+#define SRC_LISP_FUNCTION_CONSTANTS_H_
+
+
+#define LISP_SUM "+"
+#define LISP_MINUS "-"
+#define LISP_MULTIPLY "*"
+#define LISP_DIVIDE "/"
+#define LISP_EQUALS "="
+#define LISP_LESS_THAN "<"
+#define LISP_GREATER_THAN ">"
+#define LISP_LIST "list"
+#define LISP_APPEND "append"
+#define LISP_CAR "car"
+#define LISP_CDR "cdr"
+#define LISP_DEFUN "defun"
+#define LISP_PRINT "print"
+#define LISP_IF "if"
+#define LISP_SETQ "setq"
+#define LISP_SYNC "sync"
+
+
+#endif /* SRC_LISP_FUNCTION_CONSTANTS_H_ */
