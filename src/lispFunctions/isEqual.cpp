5dea13d4bff413a590a773d6a71c69139104096a
Martin Stancanelli <mastanca14@gmail.com>
2016-04-02 16:39:04 -0300

    Add isEqual class

---
+++ src/lispFunctions/isEqual.cpp
@@ -0,0 +1,16 @@
+/*
+ * Equals.cpp
+ *
+ *  Created on: Apr 2, 2016
+ *      Author: mastanca
+ */
+
+#include "isEqual.h"
+
+void isEqual::evaluate() {
+	this->result = (this->arg1 == this->arg2);
+}
+
+isEqual::~isEqual() {
+}
+
