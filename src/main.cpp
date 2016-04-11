05c0206f59fbbea73dd44c872b644013ef9f2db2
Martin Stancanelli <mastanca14@gmail.com>
2016-04-10 23:01:18 -0300

    Add final test and is passing, lets move forward

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,12 @@
 #include "Tokenizer.h"
 
 int main(){
-	std::string testing("(+ (+ 2 (- 10 7)) (* 2 2))"); // 9	// Not passing
-//	std::string testing = "(- 25 (* 5 4) (/ 50 10))"; // 20	// Not passing
+//	std::string testing("(+ (+ 2 (- 10 7)) (* 2 2))"); // 9	// Is ok
+//	std::string testing = "(- 25 (* 5 4) (/ 50 10))"; // 20	// Is ok
 //	std::string testing  = "(+ 5 5 (- 2 (+ 0 1)))"; // 11	// Is ok
 //	std::string testing = "(- 25 (* 5 4))"; // 5 // Is ok
 //	std::string testing = "(+ 5 5 5)";	// 15// Is ok
+	std::string testing = "(+ (* 2 2) (/ 50 (+ 2 (+ 2 1))))"; // 14 // Is ok
 	std::cout << "Original string: " << testing << std::endl;
 	LispParser aLispParser;
 	std::string parsedLine(aLispParser.prepareLineForParsing(&testing));
