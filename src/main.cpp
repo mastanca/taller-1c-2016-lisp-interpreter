b865c44f2eba7504224b9c9b89b8d20dd660e279
Martin Stancanelli <mastanca14@gmail.com>
2016-04-21 22:50:59 -0300

    Move errors to cout

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@
 int main(int argc, char *argv[]){
 	if (argc > 1){
 		argv++; // Using argv just to satisfy compiler
-		std::cerr << "ERROR: argumentos" << std::endl;
+		std::cout << "ERROR: argumentos" << std::endl;
 		return INPUT_ARGUMENTS_ERROR;
 	}
 	LispParser aLispParser;
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]){
 		tokensVector = tokenizer.tokenize(parsedLine);
 		LispLineValidator validator = LispLineValidator(tokensVector);
 		if (validator.isValidLine() == false){
-			std::cerr << "ERROR: <linea invalida>" << std::endl;
+			std::cout << "ERROR: " << aLispLine << std::endl;
 			return INVALID_LISP_LINE_ERROR;
 		} else {
 			aLispParser.setLispLine(tokensVector);
